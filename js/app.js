ca1cb004c71dadd68e142dbe3a2217e5c286aa5e
sigilworks <tj@sigilworks.com>
2014-02-05 16:53:09 -0500

    New front screen operational, with choices between three standard presets, and a custom, user-designed board.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
-var Gameboard = require('./gameboard');
+var Gameboard = require('./gameboard'),
+
+    mineableSpaces = function(dim) { return Math.pow(dim, 2) - 1; },
+    disableOption = function($el, undo) {
+        if (undo == null) undo = false;
+        $el[undo ? 'removeClass' : 'addClass']('disabled');
+        $el.find("input").prop('readonly', !undo);
+    },
+    enableOption = function($el) { return disableOption($el, true); },
+    Modes = { PRESET: "P", CUSTOM: "C" },
+    PresetLevels = { BEGINNER: "B", INTERMEDIATE: "I", EXPERT: "E" },
+    PresetSetups = {
+        BEGINNER: { dimensions: 9, mines: 9 },
+        INTERMEDIATE: { dimensions: 12, mines: 21 },
+        EXPERT: { dimensions: 15, mines: 67 }
+    };
 
 $(function(){
 
-    var mineableSpaces = function(dim) { return Math.pow(dim, 2) - 1; },
-        $possibleMines = $("#mine-count").siblings(".advice").find("span"),
-        disableOption = function($el) { $el.addClass('disabled'); $el.find("input").prop('readonly', true); };
-
+    var $possibleMines = $("#mine-count").siblings(".advice").find("span");
     // setting initial value
     $possibleMines.html(mineableSpaces($("#dimensions").attr("placeholder")));
+
+    $("#preset-mode").on('click', function() { enableOption($("ul.preset")); disableOption($("ul.custom")); }).click();
+    $("#custom-mode").on('click', function() { enableOption($("ul.custom")); disableOption($("ul.preset")); });
 
     // onkeyup when choosing gameboard dimensions,
     // neighboring input should mirror new value,
@@ -23,10 +38,23 @@ $(function(){
 
     $("form").on("submit", function() {
 
-        window.gameboard = new Gameboard({
-            dimensions: $("#dimensions").val(),
-            mines: $("#mine-count").val()
-        }).render();
+        var mode = $("[name=mode-select]:checked").val(),
+            gameOptions = {};
+
+        if (mode === Modes.PRESET) {
+            var level = $("[name=preset-level]:checked").val(),
+                setup = Object.keys(PresetLevels)
+                              .filter(function(pl) { return PresetLevels[pl] === level; })[0];
+
+            gameOptions.dimensions = PresetSetups[setup].dimensions;
+            gameOptions.mines = PresetSetups[setup].mines;
+        } else {
+            // Modes.CUSTOM...
+            gameOptions.dimensions = $("#dimensions").val();
+            gameOptions.mines = $("#mine-count").val();
+        }
+
+        window.gameboard = new Gameboard(gameOptions).render();
 
         $("#options-card").hide();
         $("#board-card").fadeIn();
