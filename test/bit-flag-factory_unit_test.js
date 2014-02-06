2539c65dfa7cae9213f048fe9b9de0b9cc714598
sigilworks <tj@sigilworks.com>
2014-02-06 16:23:44 -0500

    Brought in Blanket.js for code coverage. Started first unit test on BitFlagFactory/BitFlags.

---
+++ test/bit-flag-factory_unit_test.js
@@ -0,0 +1,28 @@
+var expect = chai.expect,
+    BitFlagFactory = require('../js/bit-flag-factory');
+
+// 1. test the BitFlagFactory...
+// 2. ...then test the BitFlags object it produces.
+
+describe("BitFlagFactory", function() {
+
+  describe("constructor", function() {
+    it("should have a default name", function() {
+      var cow = new Cow();
+      expect(cow.name).to.equal("Anon cow");
+    });
+
+    it("should set cow's name if provided", function() {
+      var cow = new Cow("Kate");
+      expect(cow.name).to.equal("Kate");
+    });
+  });
+
+  describe("#greets", function() {
+    it("should greet passed target", function() {
+      var greetings = (new Cow("Kate")).greets("Baby");
+      expect(greetings).to.equal("Kate greets Baby");
+    });
+  });
+
+});
