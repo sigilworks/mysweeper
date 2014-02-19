71629831cce4e404e30940f71b1a621731241c9c
sigilworks <tj@sigilworks.com>
2014-02-19 11:46:53 -0500

    Got ThemeStyler working and light/dark css file shells ready for separation.

--- js/theme-styler.js
+++ js/theme-styler.js
@@ -1,12 +1,31 @@
 var $C = require('./constants');
 
 var ThemeStyler = {
-
 	set: function(theme, $el) {
+
 		$el || ($el = $($C.DefaultConfig.board));
+
+		var themeFile = $C.Themes[theme],
+			$head = $el.parents("body").siblings("head"),
+			$styles = $head.find("link"),
+
+			hasPreExisting = function(stylesheets) {
+				return !!stylesheets.filter(function() {
+					return !!~$(this).attr('href').indexOf(themeFile);
+				}).length
+			},
+			// build a new <link> tag for the desired theme stylesheet:
+			$link = $("<link />", {
+				rel: 'stylesheet',
+				type: 'text/css',
+				href: 'css/' + themeFile + '.css'
+			});
 		// using $el as anchor to the DOM, go up and
 		// look for light.css or dark.css, and--if necessary--swap
 		// it out for `theme`.
+		// Add $link iff it doesn't already exist!
+		if (!hasPreExisting($styles))
+			$styles.after($link);
 	}
 };
 
