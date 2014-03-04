03db4c4f3c813b2cfaa592e66aa249feb4e94923
sigilworks <tj@sigilworks.com>
2014-03-04 12:24:36 -0500

    Renamed method to be accurate. Changes to scoreboard's updating of score chips.

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -13,22 +13,24 @@ function Scoreboard(score, el) {
 
 Scoreboard.prototype = {
     constructor: Scoreboard,
-    _increment: function($chip, newval) {
+    _increment: function(chips) {
         var FX_DURATION = 800;
 
-        $chip.wrapInner("<span/>")
-             .find("span")
-             .slideUp({
-                duration: FX_DURATION,
-                done: function() { $(this).parent().html(newval); }
-             });
+        chips.forEach(function(chip) {
+            var $chip = chip[0], pts = chip[1];
+
+            if ($chip.html() !== pts)
+                $chip
+                    .wrapInner("<span/>")
+                    .find("span")
+                    .delay(FX_DURATION)
+                    .slideUp(FX_DURATION, function() { $(this).parent().html(pts) });
+        }, this);
     },
     update: function(points) {
         if (!points) return;
         var pts = toStringArray(points);
-        this._increment(this.$R, pts[2]);
-        this._increment(this.$M, pts[1]);
-        this._increment(this.$L, pts[0]);
+        this._increment([[this.$R, pts[2]], [this.$M, pts[1]], [this.$L, pts[0]]]);
     }
 };
 
@@ -38,13 +40,13 @@ function toStringArray(n) {
     var num = String(n),
         len = num.length,
         DIGITS_MAX = 3,
-        OUT_OF_RANGE = "999";
-    
+        OUT_OF_RANGE = "MAX";
+
     // too big for *this* scoreboard...
     if (len > DIGITS_MAX) {
-        num = OUT_OF_RANGE; 
-        len = OUT_OF_RANGE.length; 
+        num = OUT_OF_RANGE;
+        len = OUT_OF_RANGE.length;
     }
-    
+
     return [ num[len - 3] || "0", num[len - 2] || "0", num[len - 1] || "0" ];
 }
