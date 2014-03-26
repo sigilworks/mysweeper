21fe0444e9233941f57a24d214e72271eb9aef05
sigilworks <tj@sigilworks.com>
2014-03-26 16:24:45 -0400

    refactoring Multimap class to have more of an ES5 syntax, removes prototype methods from enumeration on instance

--- js/lib/multimap.js
+++ js/lib/multimap.js
@@ -4,19 +4,18 @@ function Multimap() {
     this._table = [];
 }
 
-Multimap.prototype = {
-    constructor: Multimap,
-    get: function(row) { return this._table[row]; },
-    set: function(row, val) { (this._table[row] || (this._table[row] = [])).push(val); },
-    forEach: function(fn) { return [].forEach.call(this.values(), fn); },
-    values: function() {
+Object.defineProperties(Multimap.prototype, {
+    get: { value: function(row) { return this._table[row]; }},
+    set: { value: function(row, val) { (this._table[row] || (this._table[row] = [])).push(val); }},
+    forEach: { value: function(fn) { return [].forEach.call(this.values(), fn); }},
+    values: { value: function() {
         var _this = this;
         return Object.keys(this._table)
                      .map(function(row) { return _this._table[row]; })
                      .reduce(function(acc, item) { return acc.concat(item); }, []);
-    },
-    clear: function() { this._table = {}; },
-    size: function() { return Object.keys(this._table).length; }
-};
+    }},
+    clear: { value: function() { this._table = {}; }},
+    size: { value: function() { return Object.keys(this._table).length; }}
+});
 
 module.exports = Multimap;
